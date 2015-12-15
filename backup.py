0faf2c7e52d63e6cea35d5982b8ebf6f0a4447d9
Kyle Frost <kylefrost@comcast.net>
2015-12-15 11:02:57 -0500

    Create backup methods

--- backup.py
+++ backup.py
@@ -14,15 +14,30 @@ import scp
 import paramiko
 import shutil
 import os
+import sys
 import tweet
+import keys
 import time
 import datetime
 
 BTIME = str(int(time.time()))
 BACKUP_FILENAME = "backup" + BTIME
-# Not traditional /tmp for size reasons, instead backup to /mnt/stor (128GB drive)
 BACKUP_DIR = "/mnt/stor/tmp/" + BACKUP_FILENAME + "/"
 SCP_DIR = "/Volumes/External HD/RPi/BACKUPS/"
+
+###################################################################
+#                          BACKUP CLASS                           #
+###################################################################
+
+class tcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Backup:
     # Handles Backup related functions
@@ -69,13 +84,20 @@ class Backup:
         except:
             self.error("Could not remove backup directory.")
 
+    def ignorePath(path):
+        def ignoref(p, files):
+            return (f for f in files if os.abspath(os.path.join(p, f)) == path)
+        return ignoref
+
     def copy(self, path, bdir):
         # Copy `path` to backup directory
+
+        self.running("Backing up " + path)
 
         if os.path.isdir(path):
             # path is a directory
             try:
-                shutil.copytree(path, self.backup_dir + bdir)
+                shutil.copytree(path, self.backup_dir + bdir, symlinks=True)
                 self.success("Backed up " + path)
             except:
                 self.error("Failed backing up " + path)
@@ -117,11 +139,118 @@ class Backup:
 
     def error(self, msg):
         # Print error message with ornament
-        print "! " + msg
+        print tcolors.FAIL + tcolors.UNDERLINE + "! " + msg + tcolors.ENDC + tcolors.ENDC
 
     def success(self, msg):
         # Print success message with ornament
-        print "✓ " + msg
+        print tcolors.OKGREEN + tcolors.BOLD + "✓ " + msg + tcolors.ENDC + tcolors.ENDC
+
+    def running(self, msg):
+        print tcolors.OKBLUE + msg + "..." + tcolors.ENDC
+
+    def get_backup_dir(self):
+        return self.backup_dir
+
+###################################################################
+#                         END BACKUP CLASS                        #
+###################################################################
+
+def backup_home(backup):
+    # Backup /home
+
+    try:
+        backup.copy("/home", "home")
+    except:
+        pass
+
+def backup_apache(backup):
+    # Backup apache2 files
+
+    try:
+        def copy_apache(p):
+            # Everything in this folder backups to the same place
+            if os.path.isdir("/etc/apache2/" + p):
+                backup.copy("/etc/apache2/" + p, "etc/apache2/" + p)
+            else:
+                backup.copy("/etc/apache2/" + p, "etc/apache2")
+
+        copy_apache("apache2.conf")
+        copy_apache("conf.d")
+        copy_apache("conf-enabled")
+        copy_apache("conf-available")
+        copy_apache("envvars")
+        copy_apache("magic")
+        copy_apache("mods-enabled")
+        copy_apache("mods-available")
+        copy_apache("sites-enabled")
+        copy_apache("sites-available")
+        copy_apache("ports.conf")
+    except:
+        pass
+
+def backup_www(backup):
+    # Backup /var/www
+
+    try:
+        backup.copy("/var/www", "var/www")
+    except:
+        pass
+
+def backup_crontab(backup):
+    # Backup /etc/crontab
+
+    try:
+        backup.copy("/etc/crontab", "etc")
+    except:
+        pass
+
+def backup_pip(backup):
+    # Backup pip packages
+
+    try:
+        subprocess.call("pip freeze > /tmp/pip.txt", shell=True)
+        backup.success("Created /tmp/pip.txt.")
+        shutil.copy2("/tmp/pip.txt", backup.get_backup_dir() + "pip.txt")
+        os.remove("/tmp/pip.txt")
+        backup.success("Removed /tmp/pip.txt.")
+    except:
+        backup.error("There was a problem backing up pip packages.")
+
+def backup_apt(backup):
+    # Backup apt packages
+
+    # TO REINSTALL
+    # sudo dpkg --set-selectins < apt.txt
+    # sudo dselect
+
+    try:
+        subprocess.call("dpkg --get-selections > /tmp/apt.txt", shell=True)
+        backup.success("Created /tmp/apt.txt.")
+        shutil.copy2("/tmp/apt.txt", backup.get_backup_dir() + "apt.txt")
+        os.remove("/tmp/apt.txt")
+        backup.success("Removed /tmp/apt.txt.")
+    except:
+        backup.error("There was a problem backing up apt packages.")
+
+def backup_mysql(backup):
+    # Backup MySQL databases
+
+    try:
+        subprocess.call("mysqldump --all-databases -p\"" + keys.MYSQL_PASS + "\" > /tmp/mysql.sql", shell=True)
+        backup.success("Created /tmp/mysql.sql.")
+        shutil.copy2("/tmp/mysql.sql", backup.get_backup_dir() + "mysql.sql")
+        os.remove("/tmp/mysql.sql")
+        backup.success("Removed /tmp/mysql.sql.")
+    except:
+        backup.error("There was a problem backing up MySQL.")
+
+def backup_mnt(backup):
+    # Backup /mnt
+
+    try:
+        backup.copy("/mnt/", "mnt")
+    except:
+        pass
 
 def main():
     # Main function
@@ -130,31 +259,47 @@ def main():
     backup = Backup()
 
     # Tweet that backup is starting
-    # cur_time = time.strftime("%-I:%M:%S %p")
-    # tweet.Tweet("@_kylefrost Starting a backup now. The time is " + cur_time + ".")
+    cur_time = time.strftime("%-I:%M:%S %p")
+    tweet.Tweet("@_kylefrost Starting a backup now. The time is " + cur_time + ".")
 
     try:
+        # Create backup directory
         backup.create_backup_dir()
 
-        # Things to backup
-        # /home
-        # /etc/apache2
-        # /var/www
-        # /etc/crontab
-        # pip packages
-        # apt packages
-        # MySQL databases
+        # Backup /home
+        backup_home(backup)
+
+        # Backup /etc/apache2 files
+        backup_apache(backup)
+
+        # Backup /var/www
+        backup_www(backup)
+
+        # Backup /etc/crontab
+        backup_crontab(backup)
+
+        # Backup pip packages
+        backup_pip(backup)
+
+        # Backup apt packages
+        backup_apt(backup)
+
+        # Backup MySQL databases
+        backup_mysql(backup)
+
+        # Backup /mnt
+        # backup_mnt(backup)
 
         # if backup.archive():
         #     backup.send("10.0.1.2", "/Volumes/External HD/RPi/BACKUPS/")
 
-        backup.remove_backup_dir()
+        # backup.remove_backup_dir()
 
         backup.success("Backup was successful.")
-        # tweet.Tweet("@_kylefrost The backup finished successfully. The backup started at " + cur_time + ".")
+        tweet.Tweet("@_kylefrost The backup finished successfully. The backup started at " + cur_time + ".")
     except:
         backup.error("Backup was unsuccessul.")
-        # tweet.Tweet("@_kylefrost The backup was unsuccessful. The backup started at " + cur_time + ".")
+        tweet.Tweet("@_kylefrost The backup was unsuccessful. The backup started at " + cur_time + ".")
  
 if __name__ == "__main__":
     main()
